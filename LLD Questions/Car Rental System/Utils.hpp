b17ff89dc97aa65a84664ee18798331d0cdb3f30
mohd <mohdanas3971@gmail.com>
2025-10-27 23:49:59 +0530

    UML added

--- LLD Questions/Car Rental System/Utils.hpp
+++ LLD Questions/Car Rental System/Utils.hpp
@@ -21,9 +21,11 @@ enum ReservationStatus
 
 struct Date
 {
+    private:
     int day;
     int month;
     int year;
+    public:
     Date() : day(1), month(1), year(1970) {}
     Date(int d, int m, int y) : day(d), month(m), year(y) {}
 };
